a84fc2fb1ddb750961ebd652b64ca685eb0a5174
sivadev <sivaranjanithiyagarajan12@gmail.com>
2024-03-13 11:03:27 +0530

    initial commit

---
+++ GetApi.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const mysql = require("mysql2");
+
+const app = express();
+const port = 5000;
+
+// Create a MySQL connection pool
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: "localhost",
+  user: "root",
+  password: "Ammu123@",
+  database: "cruddb", // Replace with your actual database name
+});
+
+// Checking the database connection
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("Error connecting to MySQL database:", err);
+  } else {
+    console.log("Connected to MySQL database");
+
+    // Release the connection
+    connection.release();
+  }
+});
+
+app.get("/", (req, res) => {
+  const sqlQuery = "SELECT * FROM users"; 
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error("Error getting connection from pool:", err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    connection.query(sqlQuery, (error, results) => {
+      connection.release();
+
+      if (error) {
+        console.error("Error executing query:", error);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+
+      res.json(results);
+    });
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
---
+++ PostApi.js
@@ -0,0 +1,100 @@
+const express = require("express");
+require("dotenv").config();
+const mysql = require("mysql2");
+
+const app = express();
+const port = 5001;
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+app.use(express.json());
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("Error connecting to MySQL database:", err);
+  } else {
+    console.log("Connected to MySQL database");
+
+    // Release the connection
+    connection.release();
+  }
+});
+
+// GET endpoint to fetch data
+app.get("/", (req, res) => {
+  const sqlQuery = "SELECT * FROM users";
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error("Error getting connection from pool:", err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    connection.query(sqlQuery, (error, results) => {
+      connection.release();
+
+      if (error) {
+        console.error("Error executing query:", error);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+
+      res.json(results);
+    });
+  });
+});
+
+// POST endpoint to insert new data
+app.post("/data-post", (req, res) => {
+  const { NAME, AGE, CITY } = req.body;
+
+  if (!NAME || !AGE || !CITY) {
+    res
+      .status(400)
+      .json({ error: "Missing required fields in the request body" });
+    return;
+  }
+
+  const sqlInsert = "INSERT INTO users (NAME, AGE, CITY) VALUES (?, ?, ?)";
+  const values = [NAME, AGE, CITY];
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error("Error getting connection from pool:", err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    connection.query(sqlInsert, values, (error, results) => {
+      connection.release();
+
+      if (error) {
+        console.error("Error executing insert query:", error);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+    //  const statusCode = res.status;
+console.log("result",results);
+
+      res.json({
+        message: "Data inserted successfully",
+        // statusCode: statusCode,'
+        // affectedRows: results.affectedRows,
+        status: 200,
+        // status:results.
+      });
+    });
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
